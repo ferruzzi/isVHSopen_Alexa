6b8e9da413db99ee4a8c32fa92a8df4f2c0011f7
miststlkr <miststlkr@gmail.com>
2018-05-05 18:42:44 -0700

    alexafied it

--- isVHSopen_alexa.py
+++ isVHSopen_alexa.py
@@ -1,25 +1,37 @@
-#! /usr/bin/python
-# coding = utf-8
+from __future__ import print_function
 
-import requests
+import json
+#import requests
+from botocore.vendored import requests
 from datetime import datetime as dt
 from datetime import date, timedelta
 
-def isit():
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    
+    response = {}
+    outputSpeech = {}
+    reprompt = {}
+    out_json = {}
+    
     r = requests.get("https://api.vanhack.ca/s/vhs/data/door.json")
     d = r.json()
-
+    
     out = "VHS has been %s" % d['value']
     if d['value'] == "open":
         out = "Yes, " + out
     elif d['value'] == "closed":
         out = "No, " + out
-
+    
     d['last_updated'] = dt.fromtimestamp(d['last_updated'])
-# DEBUG
-#    d['last_updated'] = d['last_updated'].replace(month=4, day=5, hour=16)
+    
+    # DEBUG
+    #    d['last_updated'] = d['last_updated'].replace(day=5, hour=16)
     
     days_since = (dt.today() - d['last_updated']).days
+    print (days_since)
     
     if (days_since < -1) or (days_since == -1 and d['last_updated'].hour > dt.now().hour):
         since = "Barry messed with the timeline"
@@ -31,9 +43,9 @@ def isit():
             since = "{hour} o'clock this {part}".format(
                                                     hour=str(d['last_updated'].hour%12),
                                                     part = "evening" if d['last_updated'].hour > 12 else "morning")
-    elif days_since == 0:
+    elif days_since in [0,1]:
         since = "yesterday"
-    elif days_since in range (3,6):
+    elif days_since in range (2,6):
         since = str(days_since) + " days ago"
     elif days_since in range(6,13):
         since = "last week"
@@ -45,7 +57,14 @@ def isit():
     else:
         out = out + " since " + since
     
-    return out
-
-if __name__ == "__main__":
-    print (isit())
+    outputSpeech['type'] = "PlainText"
+    outputSpeech['text'] = out
+    
+    response['outputSpeech'] = outputSpeech
+    response['shouldEndSession'] = "False"
+    
+    out_json['version'] = "1.0"
+    out_json['response'] = response
+    out_json['sessionAttributes'] = {}
+    
+    return out_json
